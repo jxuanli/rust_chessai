35cee0b9599ad5688381352b15f786d7d9255d2f
junxua01 <junxua01@students.cs.ubc.ca>
2022-06-11 09:14:24 -0700

    finished square.rs

---
+++ src/square.rs
@@ -0,0 +1,22 @@
+mod piece;
+use piece::Piece;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Square {
+    piece: Option<Piece>,
+}
+
+pub const EMPTY_SQUARE: Square = Square { piece: None };
+
+impl From<Piece> for Square {
+    fn from(piece: Piece) -> Self { Self { piece: Some(piece) } }
+}
+
+impl Square {
+    #[inline]
+    pub fn is_empty(&self) -> bool { self.piece.is_none() }
+
+    #[inline]
+    pub fn get_piece(&self) -> Option<Piece> { self.piece }
+}
+
